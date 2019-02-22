b5f41c5023f90526cb0de98bfd2034a5962924de
Jacob Ashcraft <jacob.ashcraft@ad.snow.edu>
2019-02-22 11:18:44 -0700

    Added Class for graph in SearchGraphs

---
+++ AA-SearchGraphs.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Jacob Ashcraft
+Graphs - Strongly Connected Components
+Advanced Algorithms
+Professor Teichert
+
+"""
+
+
+class Graph:
+    def __init__(self, al):
+        self.graph = al
+
+
+if __name__ == "__main__":
+    print ("Hello World!")
