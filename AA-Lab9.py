5eccadc6c29dece1217af799bcb1be63f18ebc0c
Jacob Ashcraft <jacob.ashcraft@ad.snow.edu>
2019-04-05 11:28:38 -0600

    working on pulp

--- AA-Lab9.py
+++ AA-Lab9.py
@@ -1,4 +1,4 @@
-from pulp import LpProblem, LpMinimize, LpMaximize, LpVariable, LpBinary, LpContinuous
+from pulp import LpProblem, LpMinimize, LpMaximize, LpVariable, LpBinary, LpContinuous, LpSolverDefault
 from collections import defaultdict
 from functools import reduce
 
@@ -12,6 +12,8 @@ def lp(mode, objective, constraints):
     prob += objective
     for c in constraints:
         prob += c
+    #LpSolverDefault.msg = 1
+    print(prob.variables)
     prob.solve()
     return prob, prob.objective.value(), dict((v.name, v.value()) for v in prob.variables())
 
@@ -27,48 +29,63 @@ def ilp_sat(expression):
     variables1 = []
     constraints = []
     andlist = []
+
     def notMethod(a):
         result = LpVariable("not" + str(len(numVar)), lowBound = 0,  upBound = 1, cat = LpBinary)
         constraints.append(result == 1 - a)
 
-    def orMethod(a, b):
-        result = LpVariable("or" + str(len(constraints)), lowBound = 0,  upBound = 1, cat = LpBinary)
-        constraints.append(result >= a)
-        constraints.append(result >= b)
-        constraints.append(result <= a + b)
-        andlist.append(result)
+    # def orMethod(a, b):
+    #     result = LpVariable("or" + str(len(constraints)), lowBound = 0,  upBound = 1, cat = LpBinary)
+    #     constraints.append(result >= a)
+    #     constraints.append(result >= b)
+    #     constraints.append(result <= a + b)
+    #     andlist.append(result)
 
-    def andMethod(a, b):
-        result = LpVariable("and" + str(len(andlist)), lowBound = 0, upBound = 1, cat = LpBinary)
-        constraints.append(result <= a)
-        constraints.append(result <= b)
-        constraints.append(result >= a + b - 1)
+    # def andMethod(a, b):
+    #     result = LpVariable("and" + str(len(numVar)), lowBound = 0, upBound = 1, cat = LpBinary)
+    #     constraints.append(result <= a)
+    #     constraints.append(result <= b)
+    #     constraints.append(result >= a + b - 1)
 
 
+    # for item in expression:
+    #     for v in item.split(','):
+    #         if(not(v in numVar)):
+    #             numVar.append(v)
+    #             temp = LpVariable(v.strip(), 0, 1, cat = LpBinary)
+    #             variables.append(temp)
+    #             variables1.append(LpVariable(v.strip(), 0, 1, cat = LpBinary))
+    #             if v[0] == '-':
+    #                 notMethod(temp)
+    #         else:
+    #             temp = numVar.index(v)
+    #             variables.append(variables1[temp])
+    #     reduce(orMethod, variables)
+    #     variables = []
+    # reduce(andMethod, andlist)
     for item in expression:
         for v in item.split(','):
-            if(not(v in numVar)):
-                numVar.append(v)
-                temp = LpVariable(v.strip(), 0, 1, cat = LpBinary)
-                variables.append(temp)
-                variables1.append(LpVariable(v.strip(), 0, 1, cat = LpBinary))
-                if(v[0] == '-' or v[0 == '_']):
-                    notMethod(temp)
+            temp = ''
+            if(v[0] == '-'):
+                temp = v[1]
             else:
-                temp = numVar.index(v)
-                variables.append(variables1[temp])
-        reduce(orMethod, variables)
-        variables = []
-    reduce(andMethod, andlist)
-
+                temp = v
+            if not(temp in numVar):
+                lpvar = LpVariable(temp.strip(), 0, 1, cat = LpBinary)
+                numVar.append(temp)
+                notMethod(lpvar)
+                constraints.append(lpvar)
+    print(numVar)
     print(constraints)
-    print(lp('max', 1, constraints))
+    #print(constraints)
+    #print(lp('max', 1, constraints))
 
 def test_ipssat():
     pass
 
 def main():
     ilp_sat(['a,b,c', '-a,b,-c', '-d,-b', '-a,c', '-b,a'])
+    #ilp_sat(['a,-b,c', 'b,c,d'])
 
 
 if __name__ == "__main__":
